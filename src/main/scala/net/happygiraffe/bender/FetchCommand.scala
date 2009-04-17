fd1c03fc92b2b70bc332caf6f241985f20f2f022
Dominic Mitchell <dom@happygiraffe.net>
2009-04-18 00:22:37 +0100

    Fix a compile error.
    This is slightly nasty as I'm using an explicit cast.  But it will all change soon.

--- src/main/scala/net/happygiraffe/bender/FetchCommand.scala
+++ src/main/scala/net/happygiraffe/bender/FetchCommand.scala
@@ -3,6 +3,7 @@ package net.happygiraffe.bender
 import com.sun.syndication.feed.synd.SyndEntry
 import com.sun.syndication.fetcher.impl.HashMapFeedInfoCache
 import com.sun.syndication.fetcher.impl.HttpURLFeedFetcher
+import java.net.URI
 import scala.collection.jcl.Conversions._
 
 object FetchCommand extends Command {
@@ -10,13 +11,14 @@ object FetchCommand extends Command {
 
   def getDescription() : String =
     "go and fetch all the feeds being monitored"
-  
+
   def respond(bot: Bender, args: String) : Iterable[String] = {
     val fetcher = new HttpURLFeedFetcher(feedInfoCache)
+    val feeds = (bot.feeder !? "list").asInstanceOf[Set[URI]];
     // Oh, this is fun.
     for {
       // Rome only supports URLs, not URIs.  *sigh #1*
-      feedUrl <- bot.feeds.map(_.toURL())
+      feedUrl <- feeds.map(_.toURL())
       // Go fetch!  Good boy!
       feed = fetcher.retrieveFeed(feedUrl)
       // Rome isn't generified. *sigh #2*
