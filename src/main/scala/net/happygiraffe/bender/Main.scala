77a6be80bf54fd954ff18d24714f011dc8e6e7a0
Dominic Mitchell <dom@happygiraffe.net>
2009-04-30 22:12:43 +0100

    Externalise configuration.
    You now have to supply a properties file on the command line, stating where to connect to.

--- src/main/scala/net/happygiraffe/bender/Main.scala
+++ src/main/scala/net/happygiraffe/bender/Main.scala
@@ -1,12 +1,22 @@
 package net.happygiraffe.bender
 
+import org.apache.commons.configuration.PropertiesConfiguration
+
 object Main {
 
   def main(args: Array[String]) = {
+    if (args.length != 1)
+      usage()
+    val cf = new PropertiesConfiguration(args(0))
     val bot = new Bender()
-    bot.setVerbose(true)
-    bot.connect("localhost", 6667)
-    bot.joinChannel("#chat")
+    bot.setVerbose(cf.getBoolean("verbose", true))
+    bot.connect(cf.getString("host"), cf.getInt("port"))
+    for (channel <- cf.getStringArray("channel"))
+      bot.joinChannel(channel)
   }
 
+  def usage() {
+    Console.err.println("usage: Main bender.properties")
+    exit(2)
+  }
 }
